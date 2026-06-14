d4a8630d05fe47c99c0546445d9383086930f2a5
MoyosoreCoder <kelanimaryam1234@gmail.com>
2026-06-14 17:56:08 +0100

    data for business logic

---
+++ backend/src/data/restaurantData.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        restaurant: "Chicken Republic",
+        area: "Ikeja",
+        address: "Opebi Road, Ikeja, Lagos",
+        budget: "medium",
+        menu: ["Chicken and Chips", "Fried Rice and Chicken", "Refuel Max"]
+    },
+    {
+        restaurant: "Sweet Sensation",
+        area: "Akowonjo",
+        address: "Akowonjo, Egbeda Road, Lagos",
+        budget: "low",
+        menu: ["Jam Doughnut", "Meat Pie", "Beefy Jollof Rice and Meat", "Chicken Pie"]
+    },
+]
+
+export default data;
